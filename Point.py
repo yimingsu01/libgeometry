b961c5e5f4b1c225a6330f4950aabf552e951f59
Yiming Su <yimingsu@uchicago.edu>
2023-02-14 16:06:59 -0600

    Added dist()

--- Point.py
+++ Point.py
@@ -1,3 +1,4 @@
+import math
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -5,4 +6,7 @@ class Point:
 
     def __str__(self):
         return f"({self.x}, {self.y})"
+
+    def dist(self, p1, p2):
+        return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
 
